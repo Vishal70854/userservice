6445090b2917c0c6d91e1151f2ca0a6395e78e3c
Vishal Singh <vishalkumar70854@gmail.com>
2023-10-24 23:47:28 +0530

    spring user service and spring security with jwt tokens

---
+++ src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
@@ -0,0 +1,178 @@
+package dev.naman.userservicetestfinal.services;
+
+import dev.naman.userservicetestfinal.dtos.UserDto;
+import dev.naman.userservicetestfinal.models.Session;
+import dev.naman.userservicetestfinal.models.SessionStatus;
+import dev.naman.userservicetestfinal.models.User;
+import dev.naman.userservicetestfinal.repositories.SessionRepository;
+import dev.naman.userservicetestfinal.repositories.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.MultiValueMapAdapter;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    private UserRepository userRepository;
+    private SessionRepository sessionRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AuthService(UserRepository userRepository, SessionRepository sessionRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.sessionRepository = sessionRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public ResponseEntity<UserDto> login(String email, String password) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isEmpty()) {
+//            return this.signUp(email, password);
+            return null;
+        }
+
+        User user = userOptional.get();
+
+        if (!bCryptPasswordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Wrong username password");
+//            return null;
+        }
+
+        String token = RandomStringUtils.randomAlphanumeric(30);
+
+        MacAlgorithm alg = Jwts.SIG.HS256; //or HS384 or HS256
+        SecretKey key = alg.key().build();
+
+//        "hello".getBytes()
+
+//        String message = "{\n" +
+//                "   \"email\": \"naman@scaler.com\",\n" +
+//                "   \"roles\": [\n" +
+//                "      \"mentor\",\n" +
+//                "      \"ta\"\n" +
+//                "   ],\n" +
+//                "   \"expirationDate\": \"23rdOctober2023\"\n" +
+//                "}";
+//        // user_id
+//        // user_email
+//        // roles
+//        byte[] content = message.getBytes(StandardCharsets.UTF_8);
+
+// Create the compact JWS:
+        // below line is to create a json for our payload in jwt
+        Map<String, Object>  jsonForJwt = new HashMap<>();
+        jsonForJwt.put("email", user.getEmail());
+        jsonForJwt.put("roles", user.getRoles());
+        jsonForJwt.put("createdAt", new Date());
+        jsonForJwt.put("expiryAt", new Date(LocalDate.now().plusDays(3).toEpochDay()));
+
+        // this will create the payload in our jwt using the above requirements like
+        // email, roles, createdAt, expiryAt
+        // thus providing a more readable way to create our json web token payload
+        // we have also provided the key and algo by which we will key our jwt encrypted i.e HS256
+        token = Jwts.builder()
+                .claims(jsonForJwt)
+                .signWith(key, alg)
+                .compact();
+//
+//compact// Parse the compact JWS:
+//        content = Jwts.parser().verifyWith(key).build().parseSignedContent(jws).getPayload();
+
+        Session session = new Session();
+        session.setSessionStatus(SessionStatus.ACTIVE);
+        session.setToken(token);
+        session.setUser(user);
+        sessionRepository.save(session);
+
+        UserDto userDto = UserDto.from(user);
+
+//        Map<String, String> headers = new HashMap<>();
+//        headers.put(HttpHeaders.SET_COOKIE, token);
+
+        MultiValueMapAdapter<String, String> headers = new MultiValueMapAdapter<>(new HashMap<>());
+        headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
+
+
+
+        ResponseEntity<UserDto> response = new ResponseEntity<>(userDto, headers, HttpStatus.OK);
+//        response.getHeaders().add(HttpHeaders.SET_COOKIE, token);
+
+        return response;
+    }
+
+    public ResponseEntity<Void> logout(String token, Long userId) {
+        Optional<Session> sessionOptional = sessionRepository.findByTokenAndUser_Id(token, userId);
+
+        if (sessionOptional.isEmpty()) {
+            return null;
+        }
+
+        Session session = sessionOptional.get();
+
+        session.setSessionStatus(SessionStatus.ENDED);
+
+        sessionRepository.save(session);
+
+        return ResponseEntity.ok().build();
+    }
+
+    public UserDto signUp(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        
+        User savedUser = userRepository.save(user);
+
+        return UserDto.from(savedUser);
+    }
+
+    public SessionStatus validate(String token, Long userId) {
+        Optional<Session> sessionOptional = sessionRepository.findByTokenAndUser_Id(token, userId);
+
+        if (sessionOptional.isEmpty()) {
+            return SessionStatus.ENDED;
+//            return null;
+        }
+
+        Session session = sessionOptional.get();
+
+        if (!session.getSessionStatus().equals(SessionStatus.ACTIVE)) {
+            return SessionStatus.ENDED;
+        }
+
+
+        Jwts.parser()
+                .build();
+
+
+//        if (!session.)
+
+        return SessionStatus.ACTIVE;
+    }
+
+}
+
+/// GET /products/1 -> abcd1234abcd
+//        /authentication/validate/abcd1234abcd>u_id=12 -> true
+// if the token is any random strong, we will first need to make
+// a db call to validate the token
+// and then another call to get the details of the user
+// auth-token%3AeyJjdHkiOiJ0ZXh0L3BsYWluIiwiYWxnIjoiSFMyNTYifQ.ewogICAiZW1haWwiOiAibmFtYW5Ac2NhbGVyLmNvbSIsCiAgICJyb2xlcyI6IFsKICAgICAgIm1lbnRvciIsCiAgICAgICJ0YSIKICAgXSwKICAgImV4cGlyYXRpb25EYXRlIjogIjIzcmRPY3RvYmVyMjAyMyIKfQ.r2FVQUCn6DNHir5AlEBT2XQMgO7aN4m3xg9zcuB-zxQ
+// auth-token%3AeyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVkQXQiOjE2OTgwNzgzNDg0NTQsInJvbGVzIjpbXSwiZXhwaXJ5QXQiOjE5NjU2LCJlbWFpbCI6Im5hbWFuQHNjYWxlci5jb20ifQ._v1af8cc1YA-cEyHlX1BASwveBiASQeteWFM8UzWxfY
